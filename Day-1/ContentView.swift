49832708669a0cc56b8acd2e4b296d2d23f710ee
Abhijeet <gajjarabhijeet@gmail.com>
2024-02-07 14:29:08 -0800

    Initial Commit

---
+++ Day-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day-1
+//
+//  Created by Abhijeet gajjar on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day-1/Day_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_1App.swift
+//  Day-1
+//
+//  Created by Abhijeet gajjar on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
